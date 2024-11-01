54081f4b83694f63420e475d70c42c97253ac1b3
Julio Herrera <42546972+jurhs2000@users.noreply.github.com>
2024-10-31 19:52:23 -0600

    Added websocket as client
    Added websocket to send the BPM value to server. Also receive the prediction change message and has the ping pong managment to keep alive the connection with the server. Also the prediction is shown in the heart rate screen.

--- src/miband.js
+++ src/miband.js
@@ -1,4 +1,4 @@
-import {ADVERTISEMENT_SERVICE, CHAR_UUIDS, UUIDS} from "./constants.js";
+import { ADVERTISEMENT_SERVICE, CHAR_UUIDS, UUIDS } from "./constants.js";
 
 function buf2hex(buffer) {
   return Array.prototype.map
@@ -73,7 +73,10 @@ export class MiBand5 {
         console.log("Set new key OK");
       } else if (cmd === "100201") {
         const number = value.slice(3);
-        console.log("Received authentication challenge: ", buf2hex(value.slice(3)));
+        console.log(
+          "Received authentication challenge: ",
+          buf2hex(value.slice(3))
+        );
         const key = aesjs.utils.hex.toBytes(this.authKey);
         const aesCbc = new aesjs.ModeOfOperation.cbc(key);
         const out = aesCbc.encrypt(new Uint8Array(number));
@@ -98,27 +101,94 @@ export class MiBand5 {
   }
 
   async measureHr() {
-    console.log("Starting heart rate measurement")
-    await this.chars.hrControl.writeValue(Uint8Array.from([0x15, 0x02, 0x00]));
-    await this.chars.hrControl.writeValue(Uint8Array.from([0x15, 0x01, 0x00]));
-    await this.startNotifications(this.chars.hrMeasure, (e) => {
-      console.log("Received heart rate value: ", e.target.value);
-      const heartRate = e.target.value.getInt16();
-      window.dispatchEvent(
-        new CustomEvent("heartrate", {
-          detail: heartRate,
-        })
-      );
-    });
-    await this.chars.hrControl.writeValue(Uint8Array.from([0x15, 0x01, 0x01]));
+    // Conectar con el servidor
+    const socket = new WebSocket("ws://127.0.0.1:2222");
+    let lastHeartRateTime = Date.now(); // Tiempo de la última recepción de datos del ritmo cardíaco
+    const PING_INTERVAL = 30000; // segundos entre pings
 
-    // Start pinging HRM
-    this.hrmTimer =
-      this.hrmTimer ||
-      setInterval(() => {
-        console.log("Pinging heart rate monitor");
-        this.chars.hrControl.writeValue(Uint8Array.from([0x16]));
-      }, 12000);
+    socket.onopen = async () => {
+      console.log("Conectado al servidor");
+
+      await this.chars.hrControl.writeValue(
+        Uint8Array.from([0x15, 0x02, 0x00])
+      );
+      await this.chars.hrControl.writeValue(
+        Uint8Array.from([0x15, 0x01, 0x00])
+      );
+
+      await this.startNotifications(this.chars.hrMeasure, async (e) => {
+        console.log("Received heart rate value: ", e.target.value);
+
+        // Actualizar el tiempo de la última recepción de datos
+        lastHeartRateTime = Date.now();
+
+        // Enviar un mensaje al servidor
+        const sensor_data = {
+          dateTime: new Date().toISOString(),
+          bpm: e.target.value.getInt16(),
+        };
+        const json_string = JSON.stringify(sensor_data);
+        console.log(json_string);
+        socket.send(json_string);
+
+        const heartRate = e.target.value.getInt16();
+        window.dispatchEvent(
+          new CustomEvent("heartrate", {
+            detail: heartRate,
+          })
+        );
+      });
+
+      await this.chars.hrControl.writeValue(
+        Uint8Array.from([0x15, 0x01, 0x01])
+      );
+
+      // Ping WebSocket cada PING_INTERVAL milisegundos
+      this.pingInterval = setInterval(() => {
+        const timeSinceLastHeartRate = Date.now() - lastHeartRateTime;
+        
+        // Si no se ha recibido un dato en más de 30 segundos
+        if (timeSinceLastHeartRate > 30000) {
+            console.warn("No se ha recibido el ritmo cardíaco en más de 30 segundos. Reiniciando...");
+            socket.send(JSON.stringify({ type: 'ping' })); // Enviar un mensaje de ping al servidor
+            
+            // Reiniciar la medición
+            this.chars.hrControl.writeValue(Uint8Array.from([0x15, 0x01, 0x01]));
+        } else {
+            console.log("Enviando ping...");
+            socket.send(JSON.stringify({ type: 'ping' })); // Enviar un ping regular
+        }
+      }, PING_INTERVAL);
+
+      socket.onmessage = function (event) {
+        // Assuming the server is sending a JSON message
+        console.log("Mensaje recibido del servidor solo event:", event);
+        console.log("Mensaje recibido del servidor:", event.data);
+        const data = JSON.parse(event.data);
+        let state = '';
+        if (data.sleepStage == 0) {
+          state = "Despierto";
+        } else if (data.sleepStage == 1) {
+          state = "Sueño ligero";
+        } else if (data.sleepStage == 2) {
+          state = "Sueño profundo";
+        }
+        console.warn(`Se detectó somnolencia!\nEstado: ${state} - Fecha y hora: ${(new Date()).toLocaleString()}`);
+        window.dispatchEvent(
+          new CustomEvent("alert", {
+            detail: data.sleepStage === 0 ? "Estado: Despierto" : "Se detectó somnolencia!\nEstado: " + state,
+          })
+        );
+      };
+    };
+
+    socket.onclose = function () {
+      console.log("Conexión WebSocket cerrada");
+    };
+
+    socket.onerror = function (error) {
+      console.error("Error en WebSocket:", error);
+    };
   }
 
   async startNotifications(char, cb) {
